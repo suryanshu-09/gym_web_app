8d85ee165c0c28e42f0f8e2c6115afd2c5cc5de2
suryanshu-09 <suryanshusaini@gmail.com>
2024-04-19 14:48:30 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gym_web_app
 
 ## Mini Project Submission for Web Programming (BCSE203E)
+
+### How to use?
+
+- After cloning the repository
+- run : `npm install --lagacy-peer-deps`
+- To run the app, run : `npm start`
+- To build the app, run: `npm run build`
+
+- The site is alternatively being hosted [here](https://662232882253cea00e88c0af--musical-nougat-ce70d3.netlify.app)
---
+++ src/utils/fetchData.js
@@ -0,0 +1,29 @@
+export const exerciseOptions = {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
+  params: {limit: '10'},
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+  params: {
+    id: 'UCE_M8A5yxnLfW0KghEeajjw',
+  },
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
+
